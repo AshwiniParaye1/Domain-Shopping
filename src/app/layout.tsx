98de2d0094a399f40b7e32395287683c3ef817a2
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2025-03-21 13:18:12 +0530

    added metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,29 @@
 
 import { Provider } from "@/components/ui/provider";
 
+import type { Metadata } from "next";
+import "./globals.css";
+const title = "Domain Shopping Cart";
+const description = "Domain Shopping Cart";
+const url = "https://domain-shopping.vercel.app/";
+
+export const metadata: Metadata = {
+  title: title,
+  description: description,
+  openGraph: {
+    title: title,
+    description: description,
+    url: url,
+    type: "website",
+    siteName: "Domain Shopping Cart"
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: title,
+    description: description
+  }
+};
+
 export default function RootLayout(props: { children: React.ReactNode }) {
   const { children } = props;
   return (
